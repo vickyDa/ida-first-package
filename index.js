74597e55f9a18968e82629a02ef52a8fe21bd4a3
A623073 <ida.gnintoungbe@atos.net>
2017-09-29 12:38:22 +0000

    my first package npm by ida

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('simple-node-package started');
+console.log('ida-first-package started');
+//module.exports = 'myExport';
+//module.exports = function(moduleArgument) {
+//    console.log('moduleArgument is', moduleArgument);
+//    return 'moduleResult';
+//};
+
+module.exports = function (base,exposant) {
+    console.log('base  is '+base + " exposant is "+ exposant);
+    this.base = base;
+    this.exposant = exposant;
+    return {
+        pow: pow.bind(this)
+    }
+};
+
+function pow() {
+    return Math.pow(this.base, this.exposant) ;
+}
+
